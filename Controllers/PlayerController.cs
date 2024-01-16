c54fc85877b51992cacbe5ad88446b4dde5fe97e
Soumya Sutar <soumsutar9@gmail.com>
2024-01-16 20:25:46 +0530

    Soumya

---
+++ Controllers/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAppPlayer.Models;
+
+namespace WebAppPlayer.Controllers
+{
+    public class PlayerController : Controller
+    {
+        static List<Player> players = new List<Player>()
+        {
+            new Player() {PId=1,PName="Virat Kohli",PCountry="RCB",PType="Batsman"},
+            new Player() {PId=2,PName="AB DEVELLIERS",PCountry="DC",PType="Batsman/WK"},
+            new Player() {PId=3,PName="Siraj",PCountry="SRH",PType="Bowler"},
+
+        };
+        public IActionResult Index()
+        {
+            return View(players);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View(new Player());
+        }
+        public IActionResult Create(Player Play)
+        {
+            if(ModelState.IsValid)
+            {
+                players.Add(Play);
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
---
+++ Models/Player.cs
@@ -0,0 +1,10 @@
+﻿namespace WebAppPlayer.Models
+{
+    public class Player
+    {
+        public int PId { get; set; }
+        public string? PName { get; set; }
+        public string? PCountry { get; set; }
+        public string? PType { get; set; }
+    }
+}
